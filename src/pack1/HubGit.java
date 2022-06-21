68845f46b7cdb97f0b54df8660cd1c07ffcd27bc
l saikumar <l saikumar@LAPTOP-QBLG0IJM>
2022-06-21 14:38:59 +0530

    share on june 21 2022 tuesday

---
+++ src/pack1/HubGit.java
@@ -0,0 +1,11 @@
+package pack1;
+
+public class HubGit {
+
+	public static void main(String[] args) {
+		System.out.println("my name is saikuamr linga");
+String name="i came from Hafizpet";
+System.out.println(name);
+	}
+
+}
